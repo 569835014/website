1b09e32e3fd7d6c31458dd70e92426228561568d
minzhang <569835014@qq.com>
2017-12-07 08:55:42 +0800

    配置文件修改

--- server/config/index.js
+++ server/config/index.js
@@ -14,26 +14,6 @@
 export default {
   "db": "mongodb://localhost/website",
   "SITE_ROOT_URL": "http://icessr.ngrok.cc",
-  "wechat": {
-    "token": "cdmacctvccav",
-    "appId": "wx3bd4658fa7c863d3",
-    "appSecret": "2b00f20a3c557d6812992b0e3562f3f1"
-  },
-  "mina":{
-    "appid": "wx3bd4658fa7c863d3",
-    "secret":"a4e90ac0fa246e6e543f0927412ac1e4"
-  },
-  "shop":{
-    "appId": "wx3bd4658fa7c863d3",
-    "partnerKey":"",
-    "mchId":""
-  },
-  "qiniu": {
-    "AK": "6khcv_DEBzqInV9q-CWI89OvtKhAaWemh-0QuVt3",
-    "SK": "W4x4QnO_1YmSpJ1t3fPhG8nJSfFo1us535mrY7Uw",
-    "URL": "ow9zt0jku.bkt.clouddn.com"
-  }
-
 }
 
 
