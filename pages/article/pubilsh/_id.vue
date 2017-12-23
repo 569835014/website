0568b3dd671758382b76ca97cde366ec0dad365c
minzhang <569835014@qq.com>
2017-12-24 01:54:23 +0800

    开发文章详情页

---
+++ pages/article/pubilsh/_id.vue
@@ -0,0 +1,31 @@
+<template>
+    <div v-html="article.content" v-if="article.content">
+
+    </div>
+</template>
+
+<script>
+  export default {
+    layout: 'Blog',
+    head(){
+
+      return {
+        meta:[{
+          "http-equiv":"keywords","content":this.article.keyWords
+        }]
+      }
+
+    },
+    async asyncData({store,params}){
+      console.info(params)
+      await store.dispatch('queryById', {id:params.id})
+      return {
+        article:store.state.article
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
