8496b26d6cd4c320c14b2c3c7442d05f4ad25685
minzhang <569835014@qq.com>
2018-03-26 21:03:22 +0800

    完善用户原型

--- server/database/schema/User.js
+++ server/database/schema/User.js
@@ -1,18 +1,111 @@
-import  BaseSchema from '../base/base'
-
-class User extends BaseSchema{
-  constructor (){
-    const config={
-      nickname:String,
-      phone:String,
-      sex:String,
-      email:String,
-      avatarUrl:String,
-      account:String,
-      password:String
+import BaseSchema from '../base/base'
+import bcryptjs from 'bcryptjs'
+const SALT_WORK_FACTOR=10;
+const Max_LOGIN_ATTEMPTS=5;
+const LOCK_TIME=2*60*60*1000
+class User extends BaseSchema {
+    constructor() {
+        const config = {
+            role:{
+                type:String,
+                default:'user'
+            },
+            phone: String,
+            account: String,
+            openid: [String],
+            unionId: String,
+            nickname: String,
+            address: String,
+            province: String,
+            country: String,
+            city: String,
+            sex: String,
+            email: String,
+            headImgUrl: String,
+            avatarUrl: String,
+            password: String,
+            hashed_password: String,
+            loginAttempts: {
+                type: Number,
+                require: true,
+                default: 0
+            },
+            lockUtil: Number,
+        }
+        super(config, "User")
     }
-    super(config,"User")
-  }
+    pre(){
+        super.pre();
+        this.schema.virtual('isLocked').get(function () {
+            return !!(this.lockUtil&&this.lockUtil>Date.now())
+        })
+        //操作数据库前做的操作
+        this.schema.pre('save',function (next) {
+            if(this.isNew){//如果是现在更新创建和更新
+                this.meta.createdAt= this.meta.updatedAt=Date.now();
+            }else{//只更新更新时间
+                this.meta.updatedAt=Date.now()
+            }
+            next();
+        });
+
+        this.schema.pre('save',function (next) {
+            let user=this;
+            if(!user.isModified('password')) return next()
+            bcryptjs.genSalt(SALT_WORK_FACTOR,(err,salt)=>{
+                if(err) return next(err)
+                bcryptjs.hash(user.password,salt,(err,hash)=>{
+                    if(err) return next(err);
+                    user.password=hash;
+                    next()
+                })
+            })
+        });
+        this.schema.methods={
+            comparePassword:function (_password,password) {
+                return new Promise((resolve,reject)=>{
+                    bcryptjs.compare(_password,password,function (err,isMatch) {
+                        if(!err) resolve(isMatch);
+                        else reject(err)
+                    })
+                })
+            },
+            incLoginAttempts:function (user) {
+                const that=this
+                return new Promise((resolve,reject)=>{
+                    if(that.lockUntil&&that.lockUntil<Date.now()){
+                        that.update({
+                            $set:{
+                                loginAttempts:1
+                            },
+                            $unset:{
+                                lockUntil:1
+                            }
+                        },function (err) {
+                            if(!err) resolve(true);
+                            else reject(err)
+                        })
+                    }else{
+                        let updates={
+                            $inc:{
+                                loginAttempts:1
+                            }
+                        }
+                        if(that.loginAttempts+1>=Max_LOGIN_ATTEMPTS&&!that.isLocked){
+                            updates.$set={
+                                lockUtil:Date.now()+LOCK_TIME
+                            }
+                        }
+                        that.update(updates,err=>{
+                            if(!err) resolve(true)
+                            else reject(err)
+                        })
+                    }
+                })
+            }
+        }
+    }
 }
-const Model=new User().entity()
+
+const Model = new User().entity()
 export default Model
