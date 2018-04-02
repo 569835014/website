044914b677ea8c980f0201f260547ba50388dd61
minzhang <569835014@qq.com>
2018-04-03 00:40:22 +0800

    node session 保持登录状态

--- server/index.js
+++ server/index.js
@@ -1,65 +1,69 @@
-import { Nuxt, Builder } from 'nuxt'
+import {Nuxt, Builder} from 'nuxt'
 import Koa from 'koa'
 import R from 'ramda'
+
 var cors = require('koa2-cors');
-import { resolve } from 'path'
-const MIDDLEWARES = ['database','common','router']
+import {resolve} from 'path'
+
+const MIDDLEWARES = ['database', 'common', 'router']
 let config = require('../nuxt.config.js')
 config.dev = !(process.env === 'production')
 const host = process.env.HOST || '127.0.0.1'
 const port = process.env.PORT || 3000
 const r = path => resolve(__dirname, path)
+
 class Server {
-  constructor () {
-    this.app = new Koa()
-    this.app.use(cors({
-      origin: function (ctx) {
-        if (ctx.url === '/test') {
-          return "*"; // 允许来自所有域名请求
-        }
-        return 'http://localhost:8080';
-      },
-      exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'],
-      maxAge: 5,
-      credentials: true,
-      allowMethods: ['GET', 'POST', 'DELETE'],
-      allowHeaders: ['Content-Type', 'Authorization', 'Accept'],
-    }))
-    this.useMiddleWares(this.app)(MIDDLEWARES)
-  }
-
-  useMiddleWares (app) {
-    return R.map(R.compose(
-      R.map(i => i(app)),
-      require,
-      i => `${r('./middlewares')}/${i}`
-    ))
-  }
-
-  async start () {
-    const nuxt = new Nuxt(config)
-    if (config.dev) {
-      const builder = new Builder(nuxt)
-      builder.build().catch(e => {
-        console.error(e) // eslint-disable-line no-console
-        process.exit(1)
-      })
+    constructor() {
+        this.app = new Koa()
+        this.app.use(cors({
+            origin: function (ctx) {
+                if (ctx.url === '/test') {
+                    return "*"; // 允许来自所有域名请求
+                }
+                return 'http://localhost:8080';
+            },
+            exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'],
+            maxAge: 5,
+            credentials: true,
+            allowMethods: ['GET', 'POST', 'DELETE'],
+            allowHeaders: ['Content-Type', 'Authorization', 'Accept'],
+        }))
+        this.useMiddleWares(this.app)(MIDDLEWARES)
     }
-    this.app.use(ctx => {
-      ctx.status = 200 // koa defaults to 404 when it sees that status is unset
 
-      return new Promise((resolve, reject) => {
-        ctx.res.on('close', resolve)
-        ctx.res.on('finish', resolve)
-        nuxt.render(ctx.req, ctx.res, promise => {
-          // nuxt.render passes a rejected promise into callback on error.
-          promise.then(resolve).catch(reject)
+    useMiddleWares(app) {
+        return R.map(R.compose(
+            R.map(i => i(app)),
+            require,
+            i => `${r('./middlewares')}/${i}`
+        ))
+    }
+
+    async start() {
+        const nuxt = new Nuxt(config)
+        if (config.dev) {
+            const builder = new Builder(nuxt)
+            builder.build().catch(e => {
+                console.error(e) // eslint-disable-line no-console
+                process.exit(1)
+            })
+        }
+        this.app.use(ctx => {
+            ctx.status = 200 // koa defaults to 404 when it sees that status is unset
+            ctx.req.session = ctx.session
+            return new Promise((resolve, reject) => {
+                ctx.res.on('close', resolve)
+                ctx.res.on('finish', resolve)
+                nuxt.render(ctx.req, ctx.res, promise => {
+                    // nuxt.render passes a rejected promise into callback on error.
+                    promise.then(resolve).catch(reject)
+                })
+            })
         })
-      })
-    })
-    this.app.listen(port, host)
-    console.log('Server listening on' + host + ':' + port)
-  }
+        this.app.listen(port, host)
+        console.log('Server listening on' + host + ':' + port)
+    }
 }
+
 const app = new Server()
 app.start()
--- store/actions.js
+++ store/actions.js
@@ -1,5 +1,4 @@
 import * as NAME from './constant'
-import axios from 'axios'
 import {queryTagList} from './action/tagsAction'
 import * as articleAction from './action/articleAction'
 import * as userAction from './action/userAction'
@@ -10,9 +9,9 @@ export default {
     nuxtServerInit({commit},{req}){
         console.info(req.session)
         if(req.session&&req.session.user){
-            const {email,nickname,avatarUrl}=req.session.user
+            const {account}=req.session.user
             const user={
-                email,nickname,avatarUrl
+                account
             }
             commit('SET_USER',user)
         }
